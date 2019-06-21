030456b26b8717fe62e9e199c24e3f661ad47154
Rustam <punk60826@gmail.com>
2019-06-21 20:38:04 +0300

    edit,blade.php

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -47,7 +47,10 @@ class CategoryController extends BaseController
      */
     public function edit($id)
     {
-        dd(__METHOD__);
+       $item = BlogCategory::findOrFail($id);
+       $categoryList = BlogCategory::all();
+
+        return view('blog.admin.category.edit', compact('item', 'categoryList'));
     }
 
     /**
@@ -59,6 +62,6 @@ class CategoryController extends BaseController
      */
     public function update(Request $request, $id)
     {
-        dd(__METHOD__);
+        dd(__METHOD__, $request->all(), $id);
     }
 }
